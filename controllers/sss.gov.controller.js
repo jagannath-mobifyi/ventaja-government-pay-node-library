b5449a642a6ef450888be2aed3e6e3b2cea267fe
Rahul Sharma <jagannath@mobifyi.com>
2021-03-24 19:29:48 +0530

    -- added ssh payment test api

--- controllers/sss.gov.controller.js
+++ controllers/sss.gov.controller.js
@@ -1,20 +1,365 @@
 
-const validateContributionSearchPrnByMember = (req, res) => {
+const validateContributionSearchPrnByMember = async(req, res) => {
     const service = req.service.sssPaymentService;
-    console.log('service', service)
-    // try {
-    //     let result = await service.validatePhilHealthPayment(req.body)
-    //     if(result.status == 200){
-    //         return res.json(result)
-    //     }else{
-    //         return res.status(result.status).send(result)
-    //     }
-    // } catch (error) {
-    //     console.log('error', error);
-    //     res.status(error.status || 500).send(error)
-    // }
+    try {
+        let result = await service.validateContributionSearchPrnByMember(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
 }
+
+const processContributionSearchPrnByMember = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processContributionSearchPrnByMember(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validateContributionSearchByPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validateContributionSearchByPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processContributionSearchByPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processContributionSearchByPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validateContributionCreateAndPayPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validateContributionCreateAndPayPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processContributionCreateAndPayPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processContributionCreateAndPayPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validateContributionPayForPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validateContributionPayForPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processContributionPayForPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processContributionPayForPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validateSearchByRtplPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validateSearchByRtplPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processSearchByRtplPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processSearchByRtplPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validatePayForRtplPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validatePayForRtplPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processPayForRtplPrn = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processPayForRtplPrn(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validateRealEstateLoanPayment = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validateRealEstateLoanPayment(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processRealEstateLoanPayment = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processRealEstateLoanPayment(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const validateMiscellaneousPayment = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.validateMiscellaneousPayment(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const processMiscellaneousPayment = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.processMiscellaneousPayment(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const inquiryContributionTransaction = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.inquiryContributionTransaction(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const inquiryShortTermLoanTransaction = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.inquiryShortTermLoanTransaction(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const inquiryRealEstateLoanTransaction = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.inquiryRealEstateLoanTransaction(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const inquiryMiscellaneousTransaction = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.inquiryMiscellaneousTransaction(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const cancelRealEstateLoanTransaction = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.cancelRealEstateLoanTransaction(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
+const cancelMiscellaneousTransaction = async(req, res) => {
+    const service = req.service.sssPaymentService;
+    try {
+        let result = await service.cancelMiscellaneousTransaction(req.body)
+        if(result.status == 200){
+            return res.json(result)
+        }else{
+            return res.status(result.status).send(result)
+        }
+    } catch (error) {
+        console.log('error', error);
+        res.status(error.status || 500).send(error)
+    }
+}
+
 
 module.exports = {
     validateContributionSearchPrnByMember,
+    processContributionSearchPrnByMember,
+
+    validateContributionSearchByPrn,
+    processContributionSearchByPrn,
+
+    validateContributionCreateAndPayPrn,
+    processContributionCreateAndPayPrn,
+
+    validateContributionPayForPrn,
+    processContributionPayForPrn,
+
+    validateSearchByRtplPrn,
+    processSearchByRtplPrn,
+
+    validatePayForRtplPrn,
+    processPayForRtplPrn,
+
+    validateRealEstateLoanPayment,
+    processRealEstateLoanPayment,
+
+    validateMiscellaneousPayment,
+    processMiscellaneousPayment,
+
+    inquiryContributionTransaction,
+    inquiryShortTermLoanTransaction,
+    inquiryRealEstateLoanTransaction,
+    inquiryMiscellaneousTransaction,
+    
+    cancelRealEstateLoanTransaction,
+    cancelMiscellaneousTransaction,
 }
--- routes/sssPayment.js
+++ routes/sssPayment.js
@@ -2,7 +2,33 @@ const express = require('express');
 const { sssPaymentController } = require('../controllers');
 const router = express.Router();
 
-router.post('/contribution/validate', sssPaymentController.validateContributionSearchPrnByMember);
+router.post('/contribution/searchprnbymember/validate', sssPaymentController.validateContributionSearchPrnByMember);
+router.post('/contribution/searchprnbymember/process', sssPaymentController.validateContributionSearchPrnByMember);
+router.post('/contribution/searchbyprn/validate', sssPaymentController.validateContributionSearchByPrn);
+router.post('/contribution/searchbyprn/process', sssPaymentController.processContributionSearchByPrn);
+router.post('/contribution/createandpay/validate', sssPaymentController.validateContributionCreateAndPayPrn);
+router.post('/contribution/createandpay/process', sssPaymentController.processContributionCreateAndPayPrn);
+router.post('/contribution/payforprn/validate', sssPaymentController.validateContributionPayForPrn);
+router.post('/contribution/payforprn/process', sssPaymentController.processContributionPayForPrn);
+
+router.post('/short-term-loan/searchbyrtplprn/validate', sssPaymentController.validateSearchByRtplPrn);
+router.post('/short-term-loan/searchbyrtplprn/process', sssPaymentController.processSearchByRtplPrn);
+router.post('/short-term-loan/payforrtplprn/validate', sssPaymentController.validatePayForRtplPrn);
+router.post('/short-term-loan/payforrtplprn/process', sssPaymentController.processPayForRtplPrn);
+
+router.post('/contribution/real-estate-loan/validate', sssPaymentController.validateRealEstateLoanPayment);
+router.post('/contribution/real-estate-loan/process', sssPaymentController.processRealEstateLoanPayment);
+
+router.post('/contribution/miscellaneous/validate', sssPaymentController.validateMiscellaneousPayment);
+router.post('/contribution/miscellaneous/process', sssPaymentController.processMiscellaneousPayment);
+
+router.post('/contribution/inquiry', sssPaymentController.inquiryContributionTransaction);
+router.post('/short-term-loan/inquiry', sssPaymentController.inquiryShortTermLoanTransaction);
+router.post('/real-estate-loan/inquiry', sssPaymentController.inquiryRealEstateLoanTransaction);
+router.post('/miscellaneous/inquiry', sssPaymentController.inquiryMiscellaneousTransaction);
+
+router.post('/real-estate-loan/cancel', sssPaymentController.inquiryRealEstateLoanTransaction);
+router.post('/miscellaneous/cancel', sssPaymentController.cancelMiscellaneousTransaction);
 
 
 module.exports = router;
